0b1b4d1315371bf078c7aed83ad2f1b2ec1cc33e
James Kim <code4life@gmail.com>
2019-03-21 23:23:54 -0400

    enforce numeric range

--- ShootsAndLadders/PlayerCountAssigner.cs
+++ ShootsAndLadders/PlayerCountAssigner.cs
@@ -12,6 +12,17 @@ namespace ShootsAndLadders
             {
                 var numberOfPlayersInput = Console.ReadLine();
                 validInput = int.TryParse(numberOfPlayersInput, out numberOfPlayers);
+
+                // just for sanity's sakes, i'm going to enforce a numeric range.
+                if (numberOfPlayers < 0 || numberOfPlayers > 10)
+                {
+                    validInput = false;
+                }
+
+                if (!validInput)
+                {
+                    Console.WriteLine("Please enter a number between 1 and 10.");
+                }
             } while (!validInput);
 
             return numberOfPlayers;
